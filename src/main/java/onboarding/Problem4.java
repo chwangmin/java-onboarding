73c9925fc7233a6f53081c50ce53bb1af421fdd2
chwangmin <44563065+chwangmin@users.noreply.github.com>
2023-07-06 23:44:02 +0900

    :sparkles: feat : Clear problem4 test
    :sparkles: feat : Clear problem4 test
    
    :sparkles: feat : Clear problem4 test
    
    :sparkles: feat : Clear problem4 test

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder tmpAnswer = new StringBuilder(word.length());
+
+        for (int i = 0; i< word.length();i++) {
+            int letter = word.charAt(i);
+            if (letter >= 65 && letter <= 77){
+                letter = 90 - (letter - 65);
+            }
+            else if (letter >= 97 && letter <= 109){
+                letter = 122 - (letter - 97);
+            }
+            else if (letter >= 78 && letter <= 90){
+                letter = 77 - (letter - 78);
+            }
+            else if (letter >= 110 && letter <= 122){
+                letter = 109 - (letter - 110);
+            }
+            tmpAnswer.append((char)letter);
+        }
+        return String.valueOf(tmpAnswer);
     }
 }
