b2aebddd7259295598df35f1d7495b06bac7ce00
chwangmin <44563065+chwangmin@users.noreply.github.com>
2023-07-06 23:08:27 +0900

    :sparkles: feat : Clear problem3 test

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.stream.Stream;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int sequenceNumber = 0; sequenceNumber <= number; sequenceNumber++) {
+            int[] digits = Stream.of(String.valueOf(sequenceNumber).split("")).mapToInt(Integer::parseInt).toArray();
+            for (int checkTSN : digits) {
+                if (checkTSN == 3 || checkTSN == 6 || checkTSN == 9){
+                    answer +=1;
+                }
+            }
+        }
         return answer;
     }
 }
