b678b8df6ce04da59116a6d522ed6f10ab0de467
chwangmin <ckm7907cb@gmail.com>
2023-07-08 02:06:05 +0900

    :sparkles: feat : Clear problem5 test

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        
+        int firstBankNote = 50000;
+
+        for (int i = 0; i < 8; i++){
+            int number = money / firstBankNote;
+            money = money - number * firstBankNote;
+            if (i % 2 == 0){
+                firstBankNote /= 5;
+            } else{
+                firstBankNote /= 2;
+            }
+            answer.add(number);
+        }
+        answer.add(money);
         return answer;
     }
 }
